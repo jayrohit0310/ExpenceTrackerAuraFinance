e51a28ef3887d4e20ce0961d14abdca748fe0214
jayrohit0310 <jayrohit0310@gmail.com>
2026-09-19 13:26:31 +0530

    fix: import MongoStore correctly to avoid TypeError on Vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').MongoStore;
 const bcrypt = require('bcryptjs');
 const path = require('path');
 const mongoose = require('mongoose');
